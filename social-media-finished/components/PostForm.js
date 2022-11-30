61f9e5411b466775248f698c97e7ea6c090b26cb
Jaward Sesay <jkef96@icloud.com>
2022-11-30 19:37:44 +0800

    post form components

---
+++ social-media-finished/components/PostForm.js
@@ -0,0 +1,38 @@
+app.component('post-form', {
+    template:
+    /*html*/
+    `<form class="post-form" @submit.prevent="onSubmit">
+    <h3>Create a post</h3>    
+    <textarea id="body" v-model="body"></textarea>  
+
+    <input class="button" type="submit" value="Post">  
+
+    </form>`,
+  data() {
+      return {
+          body: '',
+          date: ''
+
+      }
+  },
+  methods: {
+      onSubmit() {
+          if (this.body == '') {
+              alert('Please type something to post')
+              return
+          }
+          const current = new Date();
+          const dateNow = `${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()} - ${current.getSeconds()+" secs ago"}`;
+        
+
+          let myPost = {
+              body: this.body,
+              date: dateNow
+          }
+          this.$emit('post-submitted', myPost)
+
+          this.body = ''
+      }
+
+  }
+})
