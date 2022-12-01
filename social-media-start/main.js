8cda2b3da0f61f1ed5b1d4637cbd6feb6df20fa8
Jaward Sesay <jkef96@icloud.com>
2022-12-01 13:43:13 +0800

    Update main.js

--- social-media-start/main.js
+++ social-media-start/main.js
@@ -1,22 +1,8 @@
 const app = Vue.createApp({
     data() {
         return {
-            name: 'Jaykef (苏杰)',
-            image: './assets/images/jaykef.jpg',
-            bio: 'Hi there! I am Jaykef, a programmer, freelancer and software engineering student at beijing institute of technology.',
-            totalPosts: '164',
-            followers: '1,105',
-            following: '285',
-            isFollowing: true,
-            posts: []
         }
     },
     computed: {
-        buttonText() {
-            return (this.isFollowing) ? 'Unfollow' : 'Follow'
-        },
-        buttonText2() {
-          return (this.isFollowing) ? 'Following' : 'Unfollowed'
-        }
       }
 })
